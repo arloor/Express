48e4764ea054938c1f6c7eeda1ea47f22279cc27
arloor <admin@arloor.com>
2017-12-16 19:58:34 +0800

    Initial commit

---
+++ HttpProxy/src/main/java/Main.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+//http://detectportal.firefox.com/success.txt
+//让以上url通过代理测试成功
+
+public class Main {
+    public static void main(String[] args) {
+        ExecutorService executor= Executors.newFixedThreadPool(20);
+        try(ServerSocket ss=new ServerSocket(2222)) {
+
+            while(true){
+                //获取浏览器socket
+                Socket localSocket=ss.accept();
+                //开启service线程
+                ThreadService serviceThread=new ThreadService(localSocket);
+               executor.submit(serviceThread);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
---
+++ HttpProxy/src/main/java/ThreadService.java
@@ -0,0 +1,212 @@
+import java.io.*;
+import java.net.*;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+public class ThreadService implements Callable<Void>{
+    Socket localSocket;
+    BufferedReader localSocketBufferReader = null;
+    BufferedWriter localSocketBufferWriter=null;
+
+    public ThreadService(Socket localSocket) {
+        this.localSocket = localSocket;
+    }
+
+    @Override
+    public Void call() {
+        List<String> requestBody= getRequstFromLocal();
+        String responseBody=sendRequest2RemoteThenGetResponse(requestBody);
+        sendResponse2Local(responseBody);
+
+        //关闭localSocket相关的writer、reader和socket本身。
+        if(localSocketBufferReader!=null){
+            try {
+                localSocketBufferReader.close();
+                localSocketBufferReader=null;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally {
+                if(localSocketBufferReader!=null){
+                    try {
+                        localSocketBufferReader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+//todo:删除
+        try {
+            localSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private void sendResponse2Local(String responseBody) {
+        try {
+            localSocketBufferWriter=new BufferedWriter(new OutputStreamWriter(localSocket.getOutputStream()));
+            localSocketBufferWriter.write(responseBody);
+            localSocketBufferWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 读取浏览器发送的请求
+     * @return
+     */
+    private List<String> getRequstFromLocal(){
+        //http请求头格式说明：
+//        POST http://localhost:8080/prac1/login HTTP/1.1/r/n
+//        Host: localhost:8080/r/n
+//        User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0/r/n
+//        Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8/r/n
+//        Accept-Language: zh-CN,en-US;q=0.7,en;q=0.3/r/n
+//        Accept-Encoding: gzip, deflate/r/n
+//        Referer: http://localhost:8080/prac1/report/r/n
+//        Content-Type: application/x-www-form-urlencoded/r/n
+//        Content-Length: 23/r/n   定义了下面的请求主体的长度，代码中使用到了这一项
+//        Cookie: JSESSIONID=EC39EBD9F492EC97A6FAB247ABAF37AB/r/n
+//        Connection: keep-alive/r/n
+//        Upgrade-Insecure-Requests: 1/r/n
+//        /r/n  请求头结束的空行，很重要，用于http协议判断请求头结束，开始读取Content-Length长的请求主体。如果Content-Length没有定义，则直接发送响应
+//        user=asd&password=asdas
+
+        //下面是读取本地浏览器发送的请求
+        List<String> requestLines=new LinkedList<>();
+
+        try {
+            localSocketBufferReader = new BufferedReader(new InputStreamReader(localSocket.getInputStream()));
+            //使用BufferedReader获取line并且在line最后加上\r\n（read到的line是没有的需要自己加上）
+            String line=null;
+            int contentLength=0;
+            while(!(line= localSocketBufferReader.readLine()).equals("")){//知道读到一个空行
+                if(line.startsWith("Content-Length:")){
+                    String contentlenStr=line.split(" ")[1];
+                    contentLength=Integer.parseInt(contentlenStr);
+                }
+                line+="\r\n";//因为readline读到的是不带回车/换行的，所以需要手动增加回车换行
+                requestLines.add(line);
+            }
+            requestLines.add("\r\n");//！！！请求头读完之后加上的那个空行，很重要
+            if(contentLength!=0){//如果有定义Content-Length，则还需要增加读取请求主体的代码。
+                char[] buff=new char[contentLength];//设置contentLength长的buff用来读请求主体
+                int num= localSocketBufferReader.read(buff);//num=contentLength
+                requestLines.add(String.valueOf(buff));
+            }
+            System.out.println("读取请求完毕");
+            //在这里br不能close，如果close了那么会视为socketclose了。这有一个潜在问题，不知道怎么办
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return requestLines;
+    }
+
+    /**
+     *
+     * @param requestBody
+     * @return
+     */
+    private String sendRequest2RemoteThenGetResponse(List<String> requestBody){
+
+        System.out.println("向远程服务器发送请求");
+        String host=null;
+        int port=80;
+        for (String line:requestBody
+             ) {
+            if(line.startsWith("Host")){
+                line=line.substring(0,line.length()-2);
+                String hostPort=line.split(" ")[1];
+                if(hostPort.indexOf(':')!=-1){
+                    host=hostPort.substring(0,hostPort.indexOf(':'));
+                    String portStr=hostPort.substring(hostPort.indexOf(':')+1);
+                    port=Integer.parseInt(portStr);
+                }else {
+                    host=hostPort;
+                }
+                break;
+            }
+        }
+        System.out.println(host+":"+port);
+        if(host!=null){
+            BufferedWriter remoteSocketBufferedWriter=null;
+            BufferedReader remoteSocketBufferedReader=null;
+            Socket remoteSocket=null;
+            try {
+                InetAddress address=InetAddress.getByName(host);
+                String hostIP=address.getHostAddress();
+                remoteSocket=new Socket(hostIP,port);
+                remoteSocketBufferedWriter=new BufferedWriter(new OutputStreamWriter(remoteSocket.getOutputStream()));
+                String requset="";
+                for (String line:requestBody
+                     ) {
+                    requset+=line;
+                }
+                System.out.println(requset);
+                remoteSocketBufferedWriter.write(requset);
+                remoteSocketBufferedWriter.flush();
+
+
+                remoteSocketBufferedReader=new BufferedReader(new InputStreamReader(remoteSocket.getInputStream()));
+                String read=null;
+                int contentLength=0;
+                StringBuffer strBuff=new StringBuffer();
+                while(!(read=remoteSocketBufferedReader.readLine()).equals("")){
+                   strBuff.append(read+"\r\n");
+                    if(read.startsWith("Content-Length:")){
+                        String contentlenStr=read.split(" ")[1];
+                        contentLength=Integer.parseInt(contentlenStr);
+                    }
+
+                }
+                strBuff.append("\r\n");
+                char[] buff=new char[contentLength];
+                remoteSocketBufferedReader.read(buff);
+
+                strBuff.append(new String(buff));
+                System.out.println(strBuff.toString());
+                System.out.println("读取响应完毕");
+                remoteSocketBufferedReader.close();
+                remoteSocketBufferedReader=null;
+                remoteSocketBufferedWriter.close();
+                remoteSocketBufferedWriter=null;
+                remoteSocket.close();
+                remoteSocket=null;
+                return strBuff.toString();
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally {
+               if(remoteSocketBufferedReader!=null){
+                   try {
+                       remoteSocketBufferedReader.close();
+                   } catch (IOException e) {
+                       e.printStackTrace();
+                   }
+               }
+                if(remoteSocketBufferedWriter!=null){
+                    try {
+                        remoteSocketBufferedWriter.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if(remoteSocket!=null){
+                    try {
+                        remoteSocket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+}
